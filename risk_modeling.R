35e7a36a053a6a3e0361db3086fd70e26b4a3f56
austingriffith94 <austgriffia@gmail.com>
2017-11-19 23:34:22 -0500

    update to read function, added first part of var

--- risk_modeling.R
+++ risk_modeling.R
@@ -7,32 +7,48 @@
 setwd("C:/Users/Austin/GIT_profile/risk_management")
 getwd()
 
+invest = 1000000 # investment per firm
 file1 = "returns_main"
 file2 = "returns_comp"
 
 # import data
 # returns are in %
 # sum return percentages by day
-# assumed equal investment in each firm
-read_func <- function(value)
+# assumed equal investment weight in each firm
+read_func <- function(value, I)
 {
   filename = paste(value, ".csv", sep="")
   ret = read.csv(filename, header=TRUE)
   
+  firms = ret[["PERMNO"]]
+  count = length(unique(firms))
+  port = count*I
    
   ret_sum = aggregate(.~DATE, data=ret, FUN=sum)
   keeps = c("RET")
   ret_sum = subset(ret_sum, select = keeps)
   ret_vec = ret_sum[["RET"]]
-  return(ret_vec)
+  
+  i = 1
+  while(i < length(ret_vec))
+  {
+    ret_vec[i] = ret_vec[i]
+    i = i + 1
+  }
+  
+  data = list("port_val" = port, "returns" = ret_vec)
+  
+  return(data)
 }
 
 #reads files, gets vector of returns
-r_vec = read_func(file1)
-rc_vec = read_func(file2)
+data_m = read_func(file1,invest)
+data_c = read_func(file2,invest)
+r_vec = data_m$returns
+r_comp = data_c$returns
+conf = 0.95
 
+vol = sd(r_vec)
+avg_ret = mean(r_vec)
 
-
-data = c(length(r_vec),mean(r_vec),sd(r_vec),252*mean(r_vec))
-names(data) = c("Total Days","Daily Return","Daily Vol","Annual Return")
-data
+parvar = abs(qnorm(1-conf,0,1)*vol)
