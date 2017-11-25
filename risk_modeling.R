f37f0c7f5af45a9857b3a1e6746b632936aadd71
austingriffith94 <austgriffia@gmail.com>
2017-11-25 02:22:17 -0500

    added plotly charts

--- risk_modeling.R
+++ risk_modeling.R
@@ -6,6 +6,9 @@
 # needs to be adjusted when on new computer
 setwd("C:/Users/Austin/GIT_profile/risk_management")
 getwd()
+library(fGarch)
+library(plotly)
+packageVersion('plotly')
 
 #---------------------functions---------------------#
 
@@ -52,13 +55,13 @@ var_calc <- function(returns,port,a)
   dol_var = abs(quantile(r_vec,1-a)*port)
   exp_short = vol*dnorm(cumdist)/(1-a)
 
-  data = list("VaR"=var, "$VaR"=dol_var, 
+  data = list("VaR" = var, "$VaR" = dol_var, 
               "Expected_Shortfall" = exp_short)
   return(data)
 }
 
 # risk metrics one day modeling
-oneday_f <- function(returns,port,hist_returns,a)
+oneday_f <- function(returns,hist_returns,a)
 {
   # initial variance using historical data
   # variance based off of previous 10 days
@@ -70,7 +73,7 @@ oneday_f <- function(returns,port,hist_returns,a)
   i = 1
   variance = c(0)
   var = c(0)
-  exp_short= c(0)
+  exp_short = c(0)
   cumdist = qnorm(1-a,0,1)
   
   while(i <= nrow(returns))
@@ -83,14 +86,14 @@ oneday_f <- function(returns,port,hist_returns,a)
     variance_0 = variance_1
     i = i + 1
   }
-  returns$m_variance = variance
+  returns$rm_variance = variance
   returns$VaR = var
   returns$ExpShort = exp_short
   return(returns)
 }
 
 # garch model
-garch_f <- function(returns,hist_returns)
+garch_f <- function(returns,hist_returns,a)
 {
   # initial variance using historical data
   # variance based off of previous 10 days
@@ -98,7 +101,6 @@ garch_f <- function(returns,hist_returns)
   variance_0 = var(hist[length(hist)-10:length(hist)])
   
   # function to solve for garch model variables
-  library(fGarch)
   x.g = garchFit(~garch(1,1),returns[["RET"]])
   summary(x.g)
   coef(x.g)
@@ -106,6 +108,9 @@ garch_f <- function(returns,hist_returns)
   # variables for loop and garch model
   i = 1
   variance = c(0)
+  var = c(0)
+  exp_short = c(0)
+  cumdist = qnorm(1-a,0,1)
   alpha = coef(x.g)[3]
   beta = coef(x.g)[4]
   omega = coef(x.g)[2]
@@ -114,10 +119,14 @@ garch_f <- function(returns,hist_returns)
   {
     variance_1 = omega + beta*variance_0 + alpha*((returns[i,"RET"])^2)
     variance[i] = variance_1
+    var[i] = -1*sqrt(variance_1)*cumdist
+    exp_short[i] = sqrt(variance_1)*dnorm(cumdist)/(1-a)
     
     variance_0 = variance_1
     i = i + 1
   }
+  returns$VaR = var
+  returns$ExpShort = exp_short
   returns$g_variance = variance
   return(returns)
 }
@@ -153,34 +162,41 @@ conf = 0.95
 # pulls var calculations
 var_m = var_calc(data_m$returns,data_m$portfolio,conf)
 var_c = var_calc(data_c$returns,data_c$portfolio,conf)
+var_m
+var_c
 
-oneday_m = oneday_f(data_m$returns,data_m$portfolio,
-                  data_mh$returns,conf)
-oneday_c = oneday_f(data_c$returns,data_c$portfolio,
-                  data_ch$returns,conf)
-garch_m = garch_f(data_m$returns,data_mh$returns)
-garch_c = garch_f(data_c$returns,data_ch$returns)
+oneday_m = oneday_f(data_m$returns,data_mh$returns,conf)
+oneday_c = oneday_f(data_c$returns,data_ch$returns,conf)
+garch_m = garch_f(data_m$returns,data_mh$returns,conf)
+garch_c = garch_f(data_c$returns,data_ch$returns,conf)
 
 #---------------------graphing---------------------#
 
 # lists with labels for histograms
-hist_returns_m = list("title" = "2005 to 2010 Returns w/ Normal Curve",
+hist_label_m = list("title" = "2005 to 2010 Returns w/ Normal Curve",
                        "xlabel" = "Daily Returns")
-hist_returns_c = list("title" = "2000 to 2010 Returns w/ Normal Curve",
+hist_label_c = list("title" = "2000 to 2010 Returns w/ Normal Curve",
                        "xlabel" = "Daily Returns") 
 
 # write histograms for historical returns
-vec_histogram(data_m$returns[["RET"]], hist_returns_m)
-vec_histogram(data_c$returns[["RET"]], hist_returns_c)
+vec_histogram(data_m$returns[["RET"]], hist_label_m)
+vec_histogram(data_c$returns[["RET"]], hist_label_c)
 
-# graph variance
+# graph variance, VaR and ES
 time = c(1:nrow(oneday_m))
 time = time/252 + 2005
-plot(time,garch_m$g_variance)
-plot(time,oneday_m$m_variance)
+data = data.frame(garch_m,time)
+data$y = data$g_variance
+names = list("main" = "Garch Model Variance Main",
+             "yaxis" = "Variance of Returns")
 
-time = c(1:nrow(oneday_c))
-time = time/252 + 2000
-plot(time,garch_c$g_variance)
-plot(time,oneday_c$m_variance)
+plot_ly(data, x = ~time, y = ~y, name = names$main, 
+        type = "scatter", mode = "lines", 
+        line = list(color = 'rgb(205, 12, 24)', width = 1.5)) %>%
+layout(title = names$name,
+       xaxis = list(title = "Years"),
+       yaxis = list (title = names$yaxis))
+
+
+
 
